d8060c3bb4e66dcb127ce5d645a2be0d32386cbf
Bas Zalmstra <zalmstra.bas@gmail.com>
2019-09-29 22:36:22 +0200

    improvement: change theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,8 +55,8 @@ module.exports = {
         name: `Mun Programming Language`,
         short_name: `Mun`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2274a5`,
+        theme_color: `#2274a5`,
         display: `minimal-ui`,
         //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
