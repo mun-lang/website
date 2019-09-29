6dbdff26aa04152c5e2cb06e3906465a8e409513
Bas Zalmstra <zalmstra.bas@gmail.com>
2019-09-29 12:57:02 +0200

    blogs

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,20 +8,20 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions
+  const { createPage } = actions
 
-    const blogPost = path.resolve(`./src/templates/post.js`)
-    const result = await graphql(
-        `
+  const blogPost = path.resolve(`./src/templates/post.js`)
+  const result = await graphql(
+    `
         {
           allMarkdownRemark(
-            sort: { fields: [frontmatter___date], order: DESC }
+            sort: { fields: [fields___date], order: DESC }
             limit: 1000
           ) {
             edges {
               node {
                 fields {
-                  slug
+                  url
                 }
                 frontmatter {
                   title
@@ -31,40 +31,50 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       `
-    )
+  )
 
-    if (result.errors) {
-        throw result.errors
-    }
+  if (result.errors) {
+    throw result.errors
+  }
 
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
+  // Create blog posts pages.
+  const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach((post, index) => {
-        const previous = index === posts.length - 1 ? null : posts[index + 1].node
-        const next = index === 0 ? null : posts[index - 1].node
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
 
-        createPage({
-            path: post.node.fields.slug,
-            component: blogPost,
-            context: {
-                slug: post.node.fields.slug,
-                previous,
-                next,
-            },
-        })
+    createPage({
+      path: post.node.fields.url,
+      component: blogPost,
+      context: {
+        url: post.node.fields.url,
+        previous,
+        next,
+      },
     })
+  })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-    const { createNodeField } = actions
+  const { createNodeField } = actions
 
-    if (node.internal.type === `MarkdownRemark`) {
-        const value = createFilePath({ node, getNode })
-        createNodeField({
-            name: `slug`,
-            node,
-            value,
-        })
-    }
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    const pathRegex = /^\/(\d{4})-(\d{1,2})-(\d{1,2})-(.*)\/$/
+    const matches = pathRegex.exec(value)
+    const url = matches ? `/blog/${matches[1]}/${matches[2]}/${matches[3]}/${matches[4]}/` : value
+    createNodeField({
+      node,
+      name: `url`,
+      value: url,
+    })
+
+    let date = matches ? new Date(Date.UTC(parseInt(matches[1]), parseInt(matches[2]) - 1, parseInt(matches[3]))) : new Date()
+    createNodeField({
+      node,
+      name: `date`,
+      value: date,
+    })
+  }
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,55 +7,55 @@ import SEO from "../components/seo"
 import "./blog.scss"
 
 class BlogIndex extends React.Component {
-    render() {
-        const { data } = this.props
-        const posts = data.allMarkdownRemark.edges
+  render() {
+    const { data } = this.props
+    const posts = data.allMarkdownRemark.edges
 
-        return (
-            <Layout>
-                <SEO title="Blog" />
-                <div id="blog">
-                    <header className="content">
-                        <h2>Blog</h2>
-                        <p>On our blog we try to write frequent updates about the development of Mun.</p>
-                    </header>
-                    <section id="posts">
-                        <div className="inner">
-                            <div className="content">
-                                {posts.map(({ node }) => {
-                                    const title = node.frontmatter.title || node.fields.slug
-                                    return (
-                                        <article>
-                                            <header>
-                                                <h4><Link to={node.fields.slug}>{title}</Link></h4>
-                                                <small>{node.frontmatter.date}, {node.frontmatter.author || "The Mun Team"}</small>
-                                            </header>
-                                            <section>
-                                                <p dangerouslySetInnerHTML={{ __html: node.frontmatter.excerpt || node.excerpt }} />
-                                            </section>
-                                        </article>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </Layout>
-        );
-    }
+    return (
+      <Layout>
+        <SEO title="Blog" />
+        <div id="blog">
+          <header className="content">
+            <h2>Blog</h2>
+            <p>On our blog we try to write frequent updates about the development of Mun.</p>
+          </header>
+          <section id="posts">
+            <div className="inner">
+              <div className="content">
+                {posts.map(({ node }) => {
+                  const title = node.frontmatter.title || node.fields.slug
+                  return (
+                    <article>
+                      <header>
+                        <h4><Link to={node.fields.url}>{title}</Link></h4>
+                        <small>{node.fields.date}, {node.frontmatter.author || "The Mun Team"}</small>
+                      </header>
+                      <section>
+                        <p dangerouslySetInnerHTML={{ __html: node.frontmatter.excerpt || node.excerpt }} />
+                      </section>
+                    </article>
+                  )
+                })}
+              </div>
+            </div>
+          </section>
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { fields: [fields___date], order: DESC }) {
       edges {
         node {
           excerpt
           fields {
-            slug
+            url
+            date(formatString: "MMMM DD, YYYY")
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
             title
             excerpt
             author
