fee500bfb1cbb109d1e5c4b22e38a2d72245093a
Bas Zalmstra <zalmstra.bas@gmail.com>
2019-09-29 22:30:14 +0200

    feat: added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,6 +118,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-148878934-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
